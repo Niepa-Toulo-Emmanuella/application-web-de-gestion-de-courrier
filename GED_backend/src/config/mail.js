785008d0375a2efe16e514e92dd80574d6830a8d
Niepa-Toulo-Emmanuella <niepaemmanuella@gmail.com>
2025-10-18 05:06:27 +0000

    Ajout mail.js

---
+++ GED_backend/src/config/mail.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error('Erreur de connexion SMTP:', error);
+  } else {
+    console.log('Connexion SMTP réussie');
+  }
+});
+
+// src/config/mail.js
+module.exports = {
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  user: process.env.SMTP_USER,
+  pass: process.env.SMTP_PASS,
+  transporter
+};
